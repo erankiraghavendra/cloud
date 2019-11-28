21e688492801224913330c9be4c76901bcae0915
erankiraghavendra <31732491+erankiraghavendra@users.noreply.github.com>
2019-11-28 11:22:34 +0530

    Update shellscript.sh

--- shellscript.sh
+++ shellscript.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
-# echo '########## yum update all ###############'
-# sudo yum update -y
-echo '########## basic webserver ##############'
-sudo yum install -y httpd
-sudo systemctl enable  httpd.service
-sudo systemctl start  httpd.service
-sudo echo '<html><head></head><body><pre><code>' > /var/www/html/index.html
-sudo hostname >> /var/www/html/index.html
-sudo echo '' >> /var/www/html/index.html
-sudo cat /etc/os-release >> /var/www/html/index.html
-sudo echo '</code></pre></body></html>' >> /var/www/html/index.html
-sudo firewall-offline-cmd --add-service=http
-sudo systemctl enable  firewalld
-sudo systemctl restart  firewalld
-
+var="Hello World"
+ 
+# Run date and hostname command and store output to shell variables
+now="$(date)"
+computer_name="$(hostname)"
+ 
+#
+# print it or use the variable
+# Variable names are case sensitive $now and $NOW are different names
+#
+echo "$var"
+echo "Current date and time : $now"
+echo "Computer name : $computer_name"
